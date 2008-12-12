580a4697f3b46f540c89c900e412c0303db73170
Jens Kohl <jens.kohl@gmail.com>
2008-12-12 17:39:16 +0100

    first commit to github

---
+++ moviesorter.php
@@ -0,0 +1,113 @@
+#!/usr/bin/php
+<?php
+/**
+ * Skript zum zusammensuchen von heruntergeladenen Serien. Weiterhin
+ * werden die gefundenen Serien umbenannt und in eine Ordnerhierarchie
+ * einsortiert. Ausserdem gibt es per Growl eine Benachrichtigung.
+ *
+ * @version 1.3 (2008-10-21)
+ * @author Jens Kohl <jens.kohl@gmail.com>
+ */
+
+// In welchen Verzeichnissen soll nach Serien gesucht werden?
+$src_array = array(
+	'/Users/jkohl/Downloads-BT',
+	'/Users/jkohl/Movies',
+	'/Users/jkohl/Movies/TV-Serien',
+	'/Volumes/TV-Serien'
+);
+
+// Umbenannte Dateien werden in eins der folgenden Verzeichnisse
+// verschoben. Genauer: In das erste Verzeichnis, in dem genug
+// Platz für die Datei ist.
+//define('DST_DIR', '/Volumes/TV-Serien');
+$dst_array = array(
+	'/Volumes/Serien',
+	'/Volumes/TV-Serien'
+);
+
+//define('DST_DIR', '/Users/jkohl/Movies/TV-Serien');
+
+/**
+ * Funktion verschriebt Dateien in in eine geparste Ordnerhierarchie
+ *
+ * @author Jens Kohl <jens.kohl@gmail.com>
+ * @param string Dateiname
+ * @param string Verzeichnisname
+ * @return string Statusnachricht
+ */
+function move_file($in_file, $dir) {
+	if (preg_match('/(.*?)(S(\d{1,2})E(\d{1,2})|(\d{1,2})x(\d{1,2})).*?\.(avi|divx|mkv|mov|wmv)$/i', $in_file, $treffer)) {
+		//var_dump($treffer);
+		
+		global $dst_array;
+		
+		$title   = $treffer[1];
+		$title   = preg_replace('/(\.|-|_)/i', ' ', $title);
+		$title	 = ucwords(trim($title));
+		$season  = ($treffer[3]) ? (int) $treffer[3] : (int) $treffer[5];
+		$episode = ($treffer[4]) ? (int) $treffer[4] : (int) $treffer[6];
+		$extension = $treffer[7];
+	
+		if (preg_match('/xvid/i', $in_file)) {
+			$extension = 'divx';
+		}
+		
+		$moved = false;
+		$filesize = filesize($dir.'/'.$in_file);
+		foreach($dst_array as $thisDest) {
+			if (disk_free_space($thisDest) < $filesize) {
+				# Ziellaufwerk zu klein
+				echo 'WARN: '.$thisDest.' verfügt nicht über genügend Platz für '.$in_file."\n";
+			}
+			else
+			{
+				# Make the directory
+				if(!file_exists($thisDest.'/'.$title.'/Season '.$season)) {
+					if (!file_exists($thisDest.'/'.$title)) {
+						mkdir($thisDest.'/'.$title);
+					}
+
+					mkdir($thisDest.'/'.$title.'/Season '.$season);
+				}
+
+				$new_file = $thisDest.'/'.$title."/Season ".$season."/".sprintf("%02dx%02d", $season, $episode)." - $title.$extension";
+				rename($dir.'/'.$in_file, $new_file);
+				echo "INFO: $title $season x $episode nach $thisDest verschoben.\n";
+				$moved = true;
+				return "Moved $title ($season x $episode)\n";
+			}
+			
+			if (!$moved) {
+				echo 'FAIL: Kein Platz für ' . $in_file ." vorhanden.\n";
+			}
+		}
+	}
+}
+
+$output = '';
+
+foreach($src_array as $thisSource) {
+	echo $thisSource . ' überprüfen…'."\n";
+	
+	if ($handle = opendir($thisSource)) {
+	    while (false !== ($file = readdir($handle))) {
+			$fullfile = $thisSource.'/'.$file;
+	        if (!is_dir($fullfile) && $file != '.' && $file != '..' && substr($file, 0, 1) != '.' ) {
+				// Wenn Datei vor weniger als 20 Minuten modifiziert wurde, dann ueberspringen
+				if (fileatime($fullfile) >= (mktime() - 60 * 20))
+					continue;
+
+				$output .= move_file($file, $thisSource);
+	        }
+	    }
+	    closedir($handle);
+	}
+}
+
+//echo $output;
+if ($output) {
+	`growlnotify -t "Filme aufgeräumt" -m "$output" -I ~/Movies/`;
+}
+
+?>
---
+++ torrentwatcher.php
@@ -0,0 +1,181 @@
+#!/usr/bin/php
+<?php
+define("FEED", "http://tvrss.net/feed/eztv/");
+define("WATCHFOLDER", "/Users/jkohl/Downloads/Watchfolder");
+define("IDFILE", '/Users/jkohl/Sites/torrentwatcher.dat');
+define("DEBUG", 0);
+
+$shows = array(
+	'/Numb3rs \d+x\d+ \[HDTV \- \w+\]/i',
+	'/CSI \d+x\d+ \[HDTV \- LOL\]/i',
+	'/How I Met Your Mother \d+x\d+ \[HDTV \- \w+\]/i',
+	'/The Big Bang Theory(.*?) \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Eureka \d+x\d+(.*?)720p(.*?)/i',
+	'/Eureka \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Flashpoint \d+x\d(.*?)/i',
+	'/Eli Stone \d+x\d(.*?)/i',
+	'/How Not to Live Your Life \d+x\d(.*?)/i',
+	'/Fringe \d+x\d+ \[720P \- HDTV \- \w+\]/i',
+	'/90210 \d+x\d+ \[HDTV \- \w+\]/i',
+	'/One Tree Hill \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Rush \d+x\d+ \[HDTV - \w+\]/i',
+	'/Gossip Girl \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Privileged \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Do Not Disturb \d+x\d+(.*?)/i',
+	'/Mad Men \d+x\d+(.*?)/i',
+	'/True Blood \d+x\d+(.*?)720p(.*?)/i',
+	'/Heroes \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Entourage \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Greys Anatomy .*?\d+x\d \[HDTV \- \w+\]/i',
+	'/Greys Anatomy .*?\d+x\d(.*?)720p(.*?)/i',
+	'/^ER \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Knight Rider 2008 \d+x\d+ \[720P \- HDTV \- \w+\]/i',
+	'/Chuck \d+x\d+ \[HDTV \- \w+\]/i',
+	'/Damages \d+x\d+/i',
+	'/Californication \d+x\d+/i',
+	'/^30 Rock \d+x\d+/i',
+	'/Private Practice \d+x\d+/i',
+	'/House \d+x\d+(.*?)720p(.*?)/i',
+	'/Boston Legal \d+x\d+/i',
+	'/The IT Crowd \d+x\d+/i'
+	);
+	
+
+function writeLog($msg) {
+	echo strftime("[%H:%M:%S] ") . $msg . "\n";
+}
+
+function download($url, $file) {
+	$sh = curl_init($url);
+	$hFile = fopen($file, 'w');
+	curl_setopt($sh, CURLOPT_FILE, $hFile);
+	curl_setopt($sh, CURLOPT_HEADER, 0);
+	curl_exec($sh);
+	curl_close($sh);
+	fclose($hFile);
+}
+
+
+if (count($argv) > 1) {
+	if ($argv[1] == '-t') {
+		$testStrings = array(
+			'Flashpoint - PREAiR 2x1 [PDTV - DINA]',
+			'Greys Anatomy 5x9 [720P - HDTV - CTU]',
+			'The Office 5x8 [HDTV - LOL]',
+			'30 Rock 3x4 [HDTV - REPACK - LOL]',
+			'ER 15x8 [HDTV - XOR]',
+			'CSI 9x7 [720P - HDTV - DIMENSION]',
+			'My Name Is Earl 4x11 [HDTV - LOL]',
+			'Smallville 8x10 [720P - HDTV - CTU]',
+			'Greys Anatomy 5x9 [HDTV - 0TV]',
+			'30 Rock 3x4 [HDTV - LOL]'
+		);
+		writeLog("Testmodus (".count($testStrings)." Test-Strings) …\n");
+		define('TEST', 1);
+	} else {
+		writeLog('Teste "'. $argv[1]."\" …");
+		foreach ($shows as $show) {
+			if (preg_match($show, $argv[1], $foo)) {
+				writeLog("MATCH \"".$show."\"");
+			}
+		}
+		die();
+	}
+} else {
+	writeLog("Durchsuche ". FEED . " nach ".count($shows)." Serien …");
+	$xml = simplexml_load_file(FEED);
+}
+
+
+// Dupletten-Filehandling
+$ids = array();
+if (DEBUG < 2 || TEST != 1) {
+if (!file_exists(IDFILE)) {
+	$fp = fopen(IDFILE, 'w+') or exit("ERROR: ID Cache kann nicht geöffnet werden. Datei: ".IDFILE."\n");
+	fwrite($fp, '0');
+	fclose($fp);
+}
+else {
+	$fp = fopen(IDFILE, 'r') or exit("ERROR: ID Cache kann nicht geöffnet werden. Datei: ".IDFILE."\n");
+	$content = fread($fp, filesize(IDFILE));
+	$ids = explode(";", $content);
+	fclose($fp);
+}
+}
+
+if (TEST == 1) {
+	$found = 0;
+	foreach($testStrings as $item) {
+		//writeLog($item);
+		foreach ($shows as $show) {
+			//writeLog($show);
+			//echo $show . ": ".$item->title."\n";
+			if (preg_match($show, $item, $foo)) {
+				writeLog("MATCH $item ::: auf $show");
+				$found++;
+				break;
+			}
+		}	
+	}
+	die("\n==> ".$found . " gematcht.\n");
+}
+
+
+foreach($xml->channel->item as $item) {
+	//echo $item->title."\n";
+	if ($item->enclosure['type'] != 'application/x-bittorrent')
+		continue;
+	
+	$valid = false;
+	foreach ($shows as $show) {
+		//echo $show . ": ".$item->title."\n";
+		if (preg_match($show, $item->title, $foo)) {
+			$valid = true;
+			break;
+		}
+	}
+	
+	if (!$valid)
+		continue;
+	
+	preg_match('/\/([0-9]+)$/i', $item->enclosure['url'], $treffer);
+	$id = $treffer[1];
+	
+	if (in_array($id, $ids))
+		continue;
+		
+	$datetime = strtotime($item->pubDate);
+	
+	if (!preg_match('/Show Name: ([A-Za-z0-9äöüß\/\-\ ]+);(.+?); Season: (\d+); Episode: (\d+)/i', $item->description, $treffer))
+		continue;
+	$title = $treffer[1];
+	$season = $treffer[3];
+	$episode = $treffer[4];
+	
+	$filename = strftime("%Y-%m-%d ", $datetime) . $title . " ".$season."x".sprintf("%02d",$episode).".torrent";
+	$filename = str_replace('/', '-', $filename);
+	$filename = str_replace('\\', '-', $filename);
+	$filename = str_replace(' ', '_', $filename);
+	$filename = WATCHFOLDER.'/'.$filename."\n";
+	
+	//download($item->enclosure['url'], $filename);
+	$url = $item->enclosure['url'];
+	writeLog("URL: ".$url);
+	$gTitle = $title . " ".$season."x".sprintf("%02d", $episode);
+	`growlnotify -t "$gTitle" -m "Neue $title-Folge als Torrent verfügbar und im Watchfolder gespeichert." -I ~/Downloads/`;
+	`curl $url --output $filename`;
+	
+	$ids[] = $id; // ID cachen
+	
+	if (DEBUG < 2)
+	{
+		// IDs cachen
+		$fp = fopen(IDFILE, 'w+') or exit("ERROR: Konnte IDs nicht cachen. Datei: ".IDFILE."\n");
+		fwrite($fp, implode(';', $ids));
+		fclose($fp);
+		// Transmission starten
+		`open /Applications/Transmission.app`;
+	}
+}
+
+?>
