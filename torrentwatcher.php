696edf319c4aeff642a1038b3f2c57b7075c43f0
Jens Kohl <jens.kohl@gmail.com>
2009-02-14 14:55:59 +0100

    Serien in config.xml übertragen, torrentwatcher daran angepasst.

--- torrentwatcher.php
+++ torrentwatcher.php
@@ -9,45 +9,11 @@ define("WATCHFOLDER", absolute_path($config->watcher->watchfolder));
 define("IDFILE", absolute_path($config->watcher->idfile));
 define("DEBUG", $config->watcher->debug->attributes()->active);
 
-$shows = array(
-	'/Numb3rs \d+x\d+ \[HDTV \- \w+\]/i',
-	'/CSI \d+x\d+ \[HDTV \- LOL\]/i',
-	'/How I Met Your Mother \d+x\d+ \[HDTV \- \w+\]/i',
-	'/The Big Bang Theory(.*?) \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Eureka \d+x\d+(.*?)720p(.*?)/i',
-	'/Eureka \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Flashpoint \d+x\d(.*?)/i',
-	'/Eli Stone \d+x\d(.*?)/i',
-	'/How Not to Live Your Life \d+x\d(.*?)/i',
-	'/Fringe \d+x\d+ \[720P \- HDTV \- \w+\]/i',
-	'/90210 \d+x\d+ \[HDTV \- \w+\]/i',
-	'/One Tree Hill \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Rush \d+x\d+ \[HDTV - \w+\]/i',
-	'/Gossip Girl \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Privileged \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Do Not Disturb \d+x\d+(.*?)/i',
-	'/Chuck \d+x\d(.*?)/i',
-	'/Mad Men \d+x\d+(.*?)/i',
-	'/True Blood \d+x\d+(.*?)720p(.*?)/i',
-	'/Heroes \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Entourage \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Greys Anatomy.*?\d+x\d \[HDTV \- \w+\]/i',
-	'/Greys Anatomy.*?\d+x\d(.*?)720p(.*?)/i',
-	'/^ER \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Knight Rider 2008 \d+x\d+ \[720P \- HDTV \- \w+\]/i',
-	'/Chuck \d+x\d+ \[HDTV \- \w+\]/i',
-	'/Damages \d+x\d+/i',
-	'/Californication \d+x\d+/i',
-	'/^30 Rock \d+x\d+/i',
-	'/Private Practice \d+x\d+/i',
-	'/House \d+x\d+(.*?)720p(.*?)/i',
-	'/Boston Legal \d+x\d+/i',
-	'/The IT Crowd \d+x\d+/i',
-	'/The Office \d+x\d+/i',
-	'/Skins \d+x\d+/i',
-	'/Hustle \d+x\d+/i',
-	'/Dollhouse \d+x\d+/i'
-	);
+// @TODO: Code gibt es nur aus Kompatibilitätsgründen
+$shows = array();
+foreach ($config->watcher->shows->show as $thisShows) {
+	$shows[] = (string)$thisShows->regexp[0];
+}
 	
 
 if (count($argv) > 1) {
