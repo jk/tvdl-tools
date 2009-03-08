e394d9db4e7f5130d5d6e4fff4282f8f4fde7fbf
Jens Kohl <jens.kohl@gmail.com>
2009-03-08 22:57:15 +0100

    Identica als Microbloging Service hinzugefuegt. Bug in der Microbloging-Klasse gefixt.

--- class.microbloging.php
+++ class.microbloging.php
@@ -20,8 +20,19 @@ class Microbloging {
 			curl_setopt($this->curl, CURLOPT_POST, 1);
 			curl_setopt($this->curl, CURLOPT_USERPWD, $authString);
 		}
-		elseif ($service == 'laconia') {
-			// NOOP
+		elseif ($service == 'identica') {
+			// Identi.ca
+			// curl -u username:password -d status="Your message here"
+			// 		-k https://identi.ca/api/statuses/update.xml
+			
+			$url = 'https://identi.ca/api/statuses/update.xml';
+			$authString = $username.":".$passwd;
+			$this->curl = curl_init();
+			curl_setopt($this->curl, CURLOPT_URL, $url);
+			curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 2);
+			curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($this->curl, CURLOPT_POST, 1);
+			curl_setopt($this->curl, CURLOPT_USERPWD, $authString);
 		}
 	}
 	
@@ -30,16 +41,18 @@ class Microbloging {
 		if (strlen($string) < $len)
 			return $string;
 
-		return substr($string, 0, $len)."…";
+		return substr($string, 0, $len-1)."…";
 	}
 	
 	function send($message) {
+		$message = short($message, 140);
 		if ($this->service == 'twitter') {
 			curl_setopt($this->curl, CURLOPT_POSTFIELDS, 'status='.$message);
 			$this->buffer = curl_exec($this->curl);
 		}
-		elseif ($this->service == 'laconia') {
-			// NOOP
+		elseif ($this->service == 'identica') {
+			curl_setopt($this->curl, CURLOPT_POSTFIELDS, 'status='.$message);
+			$this->buffer = curl_exec($this->curl);
 		}
 		
 		return $this->buffer;
